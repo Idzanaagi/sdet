f36648165e8627ea409781277fad460fc8af8a03
idzanaagi <anmorgunowa@gmail.com>
2023-03-16 20:58:16 +0300

    fix: update WebdriverSetting (set up ChromeFriver options)

--- src/test/java/webdriver/WebdriverSetting.java
+++ src/test/java/webdriver/WebdriverSetting.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,8 +12,11 @@ public class WebdriverSetting {
     public WebDriver driver;
     @BeforeEach
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\on8d\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\on8d\\Desktop\\driver\\chromedriver_win32_11\\chromedriver.exe");
+        driver = new ChromeDriver(options);
         WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     @AfterEach
